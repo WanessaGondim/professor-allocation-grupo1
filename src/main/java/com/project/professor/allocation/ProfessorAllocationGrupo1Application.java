3532d0556bdbaca534514dbe1ae010dac92ff49c
Wanessa Gondim <wanessa.gondim14@gmail.com>
2022-08-19 16:07:58 -0300

    Initial project commit

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationGrupo1Application.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationGrupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationGrupo1Application.class, args);
+	}
+
+}
