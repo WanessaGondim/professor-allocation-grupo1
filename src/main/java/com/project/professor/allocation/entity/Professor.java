414d82261b364c08e71ec697a3bb8ccd5db7bd4f
loream <evelinelorena@gmail.com>
2022-08-21 21:25:00 -0300

    done professor

--- src/main/java/com/project/professor/allocation/entity/Professor.java
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Professor {
@@ -12,53 +14,68 @@ public class Professor {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(nullable = false)
 	private String name;
-	
+
 	@Column(nullable = false, unique = true)
 	private String cpf;
-	
+
 	@Column(nullable = false)
 	private Long departmentId;
-	
-		
+
+	@ManyToOne
+	@JoinColumn(name = "department_id", nullable = false, insertable = false, updatable = false)
+	private Department department;
+
 	public Professor() {
 		super();
 	}
-	
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public Long getDepartmentId() {
 		return departmentId;
 	}
+
 	public void setDepartmentId(Long departmentId) {
 		this.departmentId = departmentId;
 	}
 
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
 
 	@Override
 	public String toString() {
-		return "Professor [id=" + id + ", name=" + name + ", cpf=" + cpf + ", departmentId=" + departmentId + "]";
+		return "Professor [id=" + id + ", name=" + name + ", cpf=" + cpf + ", departmentId=" + departmentId
+				+ ", department=" + department + "]";
 	}
-	
-	
-	
+
 }
