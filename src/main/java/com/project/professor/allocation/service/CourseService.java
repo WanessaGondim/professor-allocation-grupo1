9398119ec2f750d24e12909e4d51d53a0ec720de
JulianaC <juliana.karinacm@gmail.com>
2022-08-26 14:00:00 -0300

    Part1 - Course,Service and Professor methods

--- src/main/java/com/project/professor/allocation/service/CourseService.java
+++ src/main/java/com/project/professor/allocation/service/CourseService.java
@@ -2,6 +2,7 @@ package com.project.professor.allocation.service;
 
 import org.springframework.stereotype.Service;
 
+import com.project.professor.allocation.entity.Course;
 import com.project.professor.allocation.repository.CourseRepository;
 
 @Service
@@ -14,5 +15,22 @@ public class CourseService {
 		this.courseRepository = courseRepository;
 	}
 	
+	public Course findById(Long id) {
+		Course course = courseRepository.findById(id).orElse(null);
+		return course;
+	}
+				
+	public Course Creat(Course department) {
+		department = courseRepository.save(department);
+		return department;
+	}
+	
+	public void deleteById (Long id) {
+		courseRepository.deleteById(id);
+	}
+	
+	public void deleteAll (Long id) {
+		courseRepository.deleteAllInBatch();
+	}
 	
 }
