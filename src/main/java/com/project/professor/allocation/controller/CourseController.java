2425de8905f9e69f91bd70d48c860e641e56e60b
Camila Prado <camilapradods@gmail.com>
2022-09-02 18:13:24 -0300

    update controller classes

--- src/main/java/com/project/professor/allocation/controller/CourseController.java
+++ src/main/java/com/project/professor/allocation/controller/CourseController.java
@@ -6,12 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.professor.allocation.entity.Course;
 import com.project.professor.allocation.service.CourseService;
 
 @RestController
+@RequestMapping(path = "/courses")
 public class CourseController {
 	
 	private final CourseService courseService;
