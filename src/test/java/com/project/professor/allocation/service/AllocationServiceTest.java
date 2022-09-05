0c7512f723498d62590d3aeec2691f608ea66300
Wanessa Gondim <wanessa.gondim14@gmail.com>
2022-09-04 21:04:14 -0300

    fixing errors

--- src/test/java/com/project/professor/allocation/service/AllocationServiceTest.java
+++ src/test/java/com/project/professor/allocation/service/AllocationServiceTest.java
@@ -10,8 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-import com.project.exceptions.HasCollisionException;
 import com.project.professor.allocation.entity.Allocation;
+import com.project.professor.allocation.exceptions.HasCollisionException;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application.properties")
