de45dce814c1dfd2de5c64feb4cc2e41690be7d4
Camila Prado <camilapradods@gmail.com>
2022-09-05 19:00:44 -0300

    AllocationController part2

--- src/main/java/com/project/professor/allocation/controller/AllocationController.java
+++ src/main/java/com/project/professor/allocation/controller/AllocationController.java
@@ -8,10 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.professor.allocation.entity.Allocation;
+import com.project.professor.allocation.entity.Department;
 import com.project.professor.allocation.service.AllocationService;
 
 @RestController
@@ -52,6 +56,33 @@ public class AllocationController {
 	public ResponseEntity<List<Allocation>> findByCourse(@PathVariable(name="course_id")Long id){
 		List<Allocation>allocation = allocationService.findByCourseId(id);
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Allocation> create(@RequestBody Allocation allocation){
+		
+		try {
+			Allocation alloc = allocationService.create(allocation);
+			return new ResponseEntity<Allocation>(alloc, HttpStatus.CREATED);
+		}catch(Exception ex) {
+			return new ResponseEntity<Allocation>(HttpStatus.BAD_REQUEST);
+		}	
+	}
+	
+	@PutMapping(path = "/{allocation_id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Allocation> update(@RequestBody Allocation allocation, 
+											 @PathVariable(name = "allocation_id", required=true) Long allocationId){
+		try {
+			allocation.setId(allocationId);
+			Allocation alloc = allocationService.update(allocation);
+			if(alloc == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} else { 
+				return new ResponseEntity<Allocation>(alloc, HttpStatus.OK);
+			}
+		} catch (Exception ex) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 	}
 	
 	@DeleteMapping(path = "/{allocation id}")
